ee5b3d3ab02691d16fba33453ea136c8b44386ab
Admin-Konstantin <beloglazov_k.s@mail.ru>
2022-06-08 23:46:28 +0300

    final version 2

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,16 +1,16 @@
 package com.example.demo.controllers;
 
 import com.example.demo.USDRate;
-import org.jsoup.Jsoup;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.jsoup.nodes.Element;
+
 
 import java.io.IOException;
 
@@ -21,11 +21,24 @@ public class MainController {
     private USDRate usdRate;
 
 
-    @GetMapping("/currencyRate")
+    @GetMapping("/usd")
     public String demo(Model model) throws IOException {
         model.addAttribute("clearUSD", usdRate.getClearUSD());
-        return "usd";
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails) principal).getUsername();
+            model.addAttribute("Username", username);
+        } else {
+            String username = principal.toString();
+            model.addAttribute("Username", username);
+        }
 
+        return "usd";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 
 }
